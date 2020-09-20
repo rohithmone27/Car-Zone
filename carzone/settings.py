b37f1469009c11d10fcaa1293489d4e1f2f92bfb
rohithmone27 <rohithmone27@gamil.com>
2020-09-20 17:38:54 +0530

    Added Admin Customization

--- carzone/settings.py
+++ carzone/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER' : 'postgres',
+        'PASSWORD' : 'Ronaldo@7',
+        'HOST' : 'localhost',
     }
 }
 
@@ -124,3 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'carzone/static'),
 ]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
